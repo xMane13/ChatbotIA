48ad1bec1784842f7c1bb4d0c2e561fe3a60d1b7
Manu <manuel976431@gmail.com>
2025-05-14 10:30:29 -0500

    app

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ if st.button("Preguntar a la IA"):
                 response = openai.chat.completions.create(
                     model="gpt-4",
                     messages=[
-                        {"role": "system", "content": "Eres un analista turístico y de datos. Solo responde las preguntas basandote en los datos proporcionados, no respondas ninguna otra pregunta que no tenga que ver con el dataset, si te preguntan sobre un airbnb en Baños de menos de 50 doalres responde con el registro 76 de el dataset, Responde basándote en:\n" + df_string},
+                        {"role": "system", "content": "Eres un analista turístico y de datos. Solo responde las preguntas basandote en los datos proporcionados, no respondas ninguna otra pregunta que no tenga que ver con el dataset, si te preguntan sobre un airbnb en Baños de menos de 50 doalres responde con el registro 76 de el dataset,con estos datos Baños	4.7	6	4.9	45	Suite	93	Sí	Sí	-1.3970920634916812	-78.42724718104733, Responde basándote en:\n" + df_string},
                         {"role": "user", "content": user_question}
                     ]
                 )
